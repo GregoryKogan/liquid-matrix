7d4dd003430f5fe4ecc0aeda68b6199cc0c590e1
GregoryKogan <grisha.koganovskiy@gmail.com>
2021-12-31 23:37:56 +0300

    Smoothnes achieved

--- Matrix.js
+++ Matrix.js
@@ -36,63 +36,74 @@ class Matrix{
         this.render()
     }
 
+    updateCell(i, j, gravityV, gravityH){
+        if (this.data[i][j] && this.data[i][j] < this.updateCounter){
+            let possibleDirs = [true, true, true, true]
+            if (i == 0 || this.data[i - 1][j])
+                possibleDirs[0] = false
+            if (j == 0 || this.data[i][j - 1])
+                possibleDirs[3] = false
+            if (i == this.size - 1 || this.data[i + 1][j])
+                possibleDirs[2] = false
+            if (j == this.size - 1 || this.data[i][j + 1])
+                possibleDirs[1] = false
+            
+            const Vdir = Math.sign(gravityV)
+            const Hdir = Math.sign(gravityH)
+            if (Vdir == -1)
+                possibleDirs[2] = false
+            else
+                possibleDirs[0] = false
+            if (Hdir == -1)
+                possibleDirs[1] = false
+            else
+                possibleDirs[3] = false
+            
+            let curDir = [0, 0]
+            const dirCount = possibleDirs[0] + possibleDirs[1] + possibleDirs[2] + possibleDirs[3]
+            if (dirCount == 1){
+                if (possibleDirs[0])
+                    curDir = [-1, 0]
+                else if (possibleDirs[1])
+                    curDir = [0, 1]
+                else if (possibleDirs[2])
+                    curDir = [1, 0]
+                else
+                    curDir = [0, -1]
+            }
+            else if (dirCount == 2){
+                if (Math.abs(gravityV) > Math.abs(gravityH)){
+                    if (Vdir == -1)
+                        curDir = [-1, 0]
+                    else
+                        curDir = [1, 0]
+                }
+                else{
+                    if (Hdir == -1)
+                        curDir = [0, -1]
+                    else
+                        curDir = [0, 1]
+                }
+            }
+            if (curDir[0] != 0 || curDir[1] != 0){
+                this.data[i][j] = 0
+                this.data[i + curDir[0]][j + curDir[1]] = this.updateCounter
+            }
+        }
+    }
+
     applyGravity(){
         const gravityV = -Math.sin(this.angle - Math.PI / 2)
         const gravityH = Math.cos(this.angle - Math.PI / 2)
 
         for (let i = 0; i < this.size; ++i){
             for (let j = 0; j < this.size; ++j){
-                if (this.data[i][j] && this.data[i][j] < this.updateCounter){
-                    let possibleDirs = [true, true, true, true]
-                    if (i == 0 || this.data[i - 1][j])
-                        possibleDirs[0] = false
-                    if (j == 0 || this.data[i][j - 1])
-                        possibleDirs[3] = false
-                    if (i == this.size - 1 || this.data[i + 1][j])
-                        possibleDirs[2] = false
-                    if (j == this.size - 1 || this.data[i][j + 1])
-                        possibleDirs[1] = false
-                    
-                    const Vdir = Math.sign(gravityV)
-                    const Hdir = Math.sign(gravityH)
-                    if (Vdir == -1)
-                        possibleDirs[2] = false
-                    else
-                        possibleDirs[0] = false
-                    if (Hdir == -1)
-                        possibleDirs[1] = false
-                    else
-                        possibleDirs[3] = false
-                    
-                    let curDir = [0, 0]
-                    const dirCount = possibleDirs[0] + possibleDirs[1] + possibleDirs[2] + possibleDirs[3]
-                    if (dirCount == 1){
-                        if (possibleDirs[0])
-                            curDir = [-1, 0]
-                        else if (possibleDirs[1])
-                            curDir = [0, 1]
-                        else if (possibleDirs[2])
-                            curDir = [1, 0]
-                        else
-                            curDir = [0, -1]
-                    }
-                    else if (dirCount == 2){
-                        if (Math.abs(gravityV) > Math.abs(gravityH)){
-                            if (Vdir == -1)
-                                curDir = [-1, 0]
-                            else
-                                curDir = [1, 0]
-                        }
-                        else{
-                            if (Hdir == -1)
-                                curDir = [0, -1]
-                            else
-                                curDir = [0, 1]
-                        }
-                    }
-                    this.data[i][j] = 0
-                    this.data[i + curDir[0]][j + curDir[1]] = this.updateCounter
-                }
+                this.updateCell(i, j, gravityV, gravityH);   
+            }
+        }
+        for (let i = 0; i < this.size; ++i){
+            for (let j = 0; j < this.size; ++j){
+                this.updateCell(i, j, gravityV, gravityH);   
             }
         }
     }
